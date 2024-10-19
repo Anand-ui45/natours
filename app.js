c4d289df1789f2004e9ba36177380f7355ee9b41
Anand-ui45 <ananddevi464@gmail.com>
2024-10-19 15:31:44 +0530

    adding helmet

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 //security security http
 
-// app.use(helmet.contentSecurityPolicy(contentSecurity.obj));
+app.use(helmet.contentSecurityPolicy(contentSecurity.obj));
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
